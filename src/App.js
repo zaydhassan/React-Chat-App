87440dd33fb1fc153fc8813074d443c256f08676
Zayd Hassan <zayd13062002@gmail.com>
2023-09-29 02:16:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import { ChatEngine } from "react-chat-engine";
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    if (!localStorage.getItem('username')) return <LoginForm />;
+    return (
+        <ChatEngine
+        height="100vh"
+        projectID="35cad37b-67e8-4eec-98c7-8ef2e2c13fcb"
+        userName="zaydhassan"
+        userSecret="123"
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+        />
+    );
+};
 
 export default App;
